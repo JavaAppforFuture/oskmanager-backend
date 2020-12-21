42730b2c1689f1ec06572b8214b1f3ba8a062198
rdx7777 <radek.jerzynski@gmail.com>
2020-12-21 11:09:41 +0000

    added Checkstyle and Jacoco dependencies & corrected all violations

--- src/main/java/pl/techlab24/OSKManager/model/Instructor.java
+++ src/main/java/pl/techlab24/OSKManager/model/Instructor.java
@@ -1,14 +1,23 @@
 package pl.techlab24.OSKManager.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @Data
